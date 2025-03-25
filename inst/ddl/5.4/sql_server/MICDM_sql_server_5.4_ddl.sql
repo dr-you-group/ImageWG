fdef06ebc0abc2ca3af45ea65b9dee086554ae67
burnout909 <newhorizon2232@gmail.com>
2025-03-26 08:45:01 +0900

    fix/varchar(10000) exceed max

--- inst/ddl/5.4/sql_server/MICDM_sql_server_5.4_ddl.sql
+++ inst/ddl/5.4/sql_server/MICDM_sql_server_5.4_ddl.sql
@@ -516,11 +516,11 @@ CREATE TABLE @cdmDatabaseSchema.image_occurrence (
             procedure_occurrence_id integer NOT NULL, 
             visit_occurrence_id integer NULL,
     		anatomic_site_concept_id integer NULL,
-    		wadors_uri varchar(10000) NULL,
-            local_path varchar(10000) NULL,
+    		wadors_uri varchar(MAX) NULL,
+            local_path varchar(MAX) NULL,
             image_occurrence_date date NOT NULL,
-    		image_study_uid varchar(10000) NOT NULL,
-    		image_series_uid varchar(10000) NOT NULL,
+    		image_study_uid varchar(MAX) NOT NULL,
+    		image_series_uid varchar(MAX) NOT NULL,
     		modality_concept_id integer NOT NULL);
 
 CREATE TABLE image_feature(
@@ -534,5 +534,5 @@ CREATE TABLE image_feature(
     			image_finding_concept_id integer NULL,
     			image_finding_id integer NULL,
     			anatomic_site_concept_id integer NULL,
-    			alg_system varchar(10000) NULL,
+    			alg_system varchar(MAX) NULL,
     			alg_datetime timestamp NULL);
